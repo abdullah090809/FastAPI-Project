afc114b8b34a1c0de548ca0957cb5e5b4d12543e
GogetaBlueMUI <muhammadabdullah090809@gmail.com>
2026-06-16 16:03:27 +0500

    Added Testing for User route

---
+++ tests/test_users.py
@@ -0,0 +1,13 @@
+from app.schemas import user
+from tests.database import client, session
+
+def test_root(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json().get('message') == "FastAPI Project"
+
+def test_create_user(client):
+    response = client.post("/users/", json={"email": "abdullah@gmail.com", "password": "abdullah1234"})
+    new_user = user.UserResponse(**response.json())
+    assert response.status_code == 201
+    assert new_user.email == "abdullah@gmail.com"
