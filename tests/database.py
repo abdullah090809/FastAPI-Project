bb4b25f33972753e5ff545649133e43bf2aeb752
GogetaBlueMUI <muhammadabdullah090809@gmail.com>
2026-06-18 14:07:08 +0500

    Added Test to test Login Route

--- tests/database.py
+++ tests/database.py
@@ -12,7 +12,7 @@ SQLALCHEMY_DATABASE_URL = f"postgresql://{setting.database_username}:{setting.da
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 TestingSessionMaker = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 
-@pytest.fixture(scope="module")
+@pytest.fixture()
 def session():
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
@@ -22,13 +22,12 @@ def session():
     finally:
         db.close()
 
-@pytest.fixture(scope="module")
+@pytest.fixture()
 def client(session):
     def override_get_db():
         try:
             yield session
         finally:
             session.close()
-
     app.dependency_overrides[get_db] = override_get_db
     yield TestClient(app)
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,10 +1,20 @@
-from app.schemas import user
+import pytest
+from app.config import setting
+from app.schemas import user, token
+from jose import jwt
 from tests.database import client, session
 
-def test_root(client):
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json().get('message') == "FastAPI Project"
+@pytest.fixture
+def create_test_user(client):
+    user_data={
+        "email": "abdullah@gmail.com",
+        "password": "abdullah1234"
+    }
+    response = client.post("/users/",json=user_data)
+    assert response.status_code == 201
+    new_user=response.json()
+    new_user["password"]="abdullah1234"
+    return new_user
 
 def test_create_user(client):
     response = client.post("/users/", json={"email": "abdullah@gmail.com", "password": "abdullah1234"})
@@ -12,6 +22,11 @@ def test_create_user(client):
     assert response.status_code == 201
     assert new_user.email == "abdullah@gmail.com"
 
-def test_login_user(client):
-    response = client.post("/login", data={"username": "abdullah@gmail.com", "password": "abdullah1234"})
+def test_login_user(client,create_test_user):
+    response = client.post("/login", data={"username": create_test_user["email"], "password": create_test_user["password"]})
+    login_response=token.Token(**response.json())
+    payload = jwt.decode(login_response.access_token, setting.secret_key, algorithms=[setting.algorithm])
+    id=payload.get("id")
+    assert id == create_test_user["id"]
+    assert login_response.token_type == "bearer"
     assert response.status_code==200
